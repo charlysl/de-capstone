ecb35dd47a8fcc45800e33be0f995916a5b4cb47
charlysl <carlossl@yahoo.com>
2022-05-28 14:56:56 +0200

    fixed path to spark standalone env

--- src/test/test_env.sh
+++ src/test/test_env.sh
@@ -3,7 +3,7 @@
 # i.e. not setting SPARK_HOME, when getOrCreating SparkSession, causes a very hard to debug:
 #	java.lang.IllegalArgumentException: requirement failed: Can only call getServletHandlers on a running MetricsSystem
 # 
-. ../../../../spark-standalone/env.sh
+. ../../spark-standalone/env.sh
 
 main=~/DataEng2022/de-capstone/src/main
 export PYTHONPATH=$(pwd)/plugins:$main/plugins/:$PYTHONPATH
