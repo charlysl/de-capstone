91347282cc85184e73179f0f6941a8ba97bc57f0
charlysl <carlossl@yahoo.com>
2022-05-28 18:42:57 +0200

    added: datalake.zip as a py_files arg

--- src/main/plugins/operators/etl_spark_operator.py
+++ src/main/plugins/operators/etl_spark_operator.py
@@ -3,8 +3,19 @@ from airflow.models.variable import Variable
 
 class ETLSparkOperator(SparkSubmitOperator):
     """
-    Based on "Unit test for custom operator" in:
-    https://airflow.apache.org/docs/apache-airflow/stable/best-practices.html
+    Prereqs:
+    - Airflow running with following env vars same/compatible as Spark cluster:
+        - JAVA_HOME (an exception thrown if not Java 8 with the 
+                    pyspark version installed in Airflow's env,
+                    which is 2.X, because 3.X breaks Airflow)
+        - SPARK_HOME (if spark-submit and server versions 
+                        different there can be non-obvious 
+                        exceptions at the server)
+    - Do not install the spark jobs directory inside the Airflow
+        operators directory, Airflow will throw many exceptions.
+    - zip datalake source files as datalake.zip, place it in 
+        the spark jobs directory, and pass it to the cluster
+        as a py_files argument
 
     NOTES:
     - One drawback is that the context can't be accessed within the constructor.
@@ -18,7 +29,7 @@ class ETLSparkOperator(SparkSubmitOperator):
         kwargs['conn_id']='spark'
         kwargs['verbose']=True
         kwargs['conf']={
-            'spark.sql.shuffle.partitions': 5,
+            'spark.sql.shuffle.partitions': 12,
             'spark.executor.memory': '1g'
         }
 
@@ -26,6 +37,8 @@ class ETLSparkOperator(SparkSubmitOperator):
             application = kwargs['application']
         else:
             application = kwargs['name']
+        
+        # application path (the python job) is relative to AIRFLOW_HOME
         kwargs['application']=f"etl_spark_jobs/etl_{application}.py"
 
         kwargs['env_vars'] = self._add_dict_pair(
@@ -33,6 +46,9 @@ class ETLSparkOperator(SparkSubmitOperator):
             'DATALAKE_ROOT',
             Variable.get('datalake_root')
         )
+
+        # upload the datalake dependencies to the spark cluster
+        kwargs['py_files'] = 'etl_spark_jobs/datalake.zip'
 
         super().__init__(*args, **kwargs)
 
