3d8fa1a97ae76aa651d0edcf202a85ac12ffe6f3
charlysl <carlossl@yahoo.com>
2022-05-31 10:01:53 +0200

    removed: obsoleted by DimFileBase

--- src/main/datalake/datamodel/route_dim.py
+++
@@ -1,4 +0,0 @@
-from datalake.model.dim import Dim
-
-class RouteDim(Dim):
-    nk = ['airline', 'flight_number', 'port_id']
--- src/main/datalake/datamodel/time_dim.py
+++
@@ -1,4 +0,0 @@
-from datalake.model.dim import Dim
-
-class TimeDim(Dim):
-    nk = ['date']
