6990a981e5ff640e08bb0091a4b0aec8d19ccc01
charlysl <carlossl@yahoo.com>
2022-06-08 11:08:08 +0200

    added: emr detection

--- src/main/datalake/utils/aws_helper.py
+++ src/main/datalake/utils/aws_helper.py
@@ -23,3 +23,24 @@ class AwsHelper:
     @staticmethod
     def secret_key():
         return AwsHelper.get_credentials().get('aws_secret_access_key')
+
+    @staticmethod
+    def is_running_in_emr():
+        """
+        Description: detect if spark job is to be ran/running in an EMR cluster
+
+        Return:
+        - If invoked outside a Spark job (i.e. from Airflow), True if
+          env var DE_CAPSTONE_IS_EMR is set.
+        _ If invoked inside a Spark job, True is env var USER == hadoop
+        """
+
+        is_emr_key = 'DE_CAPSTONE_IS_EMR'
+        if is_emr_key in os.environ:
+            return os.environ[is_emr_key].lower() == 'true'
+
+        #There are many other environment variables that could have 
+        # been checked instead.
+        key = 'USER'
+        return key in os.environ and os.environ[key] == 'hadoop'
+
